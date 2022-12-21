6550d5cb064d828734fc34719c1f12a59d452293
André Nunes <31810024+WodX@users.noreply.github.com>
2022-12-21 15:43:15 +0000

    Fix assets precompile issue (#4)

--- lib/live_log/engine.rb
+++ lib/live_log/engine.rb
@@ -1,5 +1,8 @@
+# frozen_string_literal: true
+
 module LiveLog
   class Engine < ::Rails::Engine
     isolate_namespace LiveLog
+    config.assets.precompile << 'live_log/application.css'
   end
 end
