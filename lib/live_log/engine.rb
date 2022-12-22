8fbfa2cf9cfc9635d30ad47f1f0110d4a6554b10
André Nunes <31810024+WodX@users.noreply.github.com>
2022-12-22 10:32:19 +0000

    Add rspec (#5)
    * Add new dummy app and remove the old one
    
    * Add rspec config
    
    * Delete .DS_Store

--- lib/live_log/engine.rb
+++ lib/live_log/engine.rb
@@ -4,5 +4,9 @@ module LiveLog
   class Engine < ::Rails::Engine
     isolate_namespace LiveLog
     config.assets.precompile << 'live_log/application.css'
+
+    config.generators do |g|
+      g.test_framework :rspec
+    end
   end
 end
--- test/live_log_test.rb
+++
@@ -1,7 +0,0 @@
-require "test_helper"
-
-class LiveLogTest < ActiveSupport::TestCase
-  test "it has a version number" do
-    assert LiveLog::VERSION
-  end
-end
