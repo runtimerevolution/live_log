5ed9cf3ce8fe37716b2fa155779ff70990556916
a-figueiredo-campos <a.campos@runtime-revolution.com>
2023-01-19 15:12:52 +0000

    Added to dummy the group in routes

--- spec/dummy/config/routes.rb
+++ spec/dummy/config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
     ActiveSupport::SecurityUtils.secure_compare(::Digest::SHA256.hexdigest(username), ::Digest::SHA256.hexdigest(ENV['LIVELOG_USERNAME'])) &
       ActiveSupport::SecurityUtils.secure_compare(::Digest::SHA256.hexdigest(password), ::Digest::SHA256.hexdigest(ENV['LIVELOG_PASSWORD']))
   end
-  mount LiveLog::Web, at: '/live_log'
+  mount LiveLog::Web, at: '/live_log', defaults: { group: 'RRTools' }
 end
